83a9cf1f34f961c265a7ef2fc7a161e8478e1e4c
cmal <{ID}+{username}@users.noreply.github.com>
2020-10-22 15:53:26 +0800

    update

---
+++ property.py
@@ -0,0 +1,216 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    # Getter function
+    @property
+    def name(self):
+        return self._name
+
+    # Setter function
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string')
+        self._name = value
+
+    # Deleter function
+    @name.deleter
+    def name(self):
+        raise AttributeError("Can't delete attribute")
+
+
+# Descriptor attribute for an integer type-checked attribute
+class Integer:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an int')
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+        
+        
+class lazyproperty:
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            value = self.func(instance)
+            setattr(instance, self.func.__name__, value)
+            return value
+
+# or unmodified version
+def lazyproperty(func):
+    name = '_lazy_' + func.__name__
+    @property
+    def lazy(self):
+        if hasattr(self, name):
+            return getattr(self, name)
+        else:
+            value = func(self)
+            setattr(self, name, value)
+            return value
+    return lazy
+
+
+class Structure1:
+    # Class variable that specifies expected fields
+    _fields = []
+
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+
+class Structure2:
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        if len(args) > len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set all of the positional arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the remaining keyword arguments
+        for name in self._fields[len(args):]:
+            setattr(self, name, kwargs.pop(name))
+
+        # Check for any remaining unknown arguments
+        if kwargs:
+            raise TypeError('Invalid argument(s): {}'.format(','.join(kwargs)))
+
+
+class Structure3:
+    # Class variable that specifies expected fields
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the additional arguments (if any)
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+
+        if kwargs:
+            raise TypeError('Duplicate values for {}'.format(','.join(kwargs)))
+
+
+
+
+from abc import ABCMeta, abstractmethod
+
+class IStream(metaclass=ABCMeta):
+    @abstractmethod
+    def read(self, maxbytes=-1):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+
+
+
+# Decorator for applying type checking
+def Typed(expected_type, cls=None):
+    if cls is None:
+        return lambda cls: Typed(expected_type, cls)
+    super_set = cls.__set__
+
+    def __set__(self, instance, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('expected ' + str(expected_type))
+        super_set(self, instance, value)
+
+    cls.__set__ = __set__
+    return cls
+
+
+# Decorator for unsigned values
+def Unsigned(cls):
+    super_set = cls.__set__
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        super_set(self, instance, value)
+
+    cls.__set__ = __set__
+    return cls
+
+
+# Decorator for allowing sized values
+def MaxSized(cls):
+    super_init = cls.__init__
+
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super_init(self, name, **opts)
+
+    cls.__init__ = __init__
+
+    super_set = cls.__set__
+
+    def __set__(self, instance, value):
+        if len(value) >= self.size:
+            raise ValueError('size must be < ' + str(self.size))
+        super_set(self, instance, value)
+
+    cls.__set__ = __set__
+    return cls
+
+
+# Specialized descriptors
+@Typed(int)
+class Integer(Descriptor):
+    pass
+
+
+@Unsigned
+class UnsignedInteger(Integer):
+    pass
+
+
+@Typed(float)
+class Float(Descriptor):
+    pass
+
+
+@Unsigned
+class UnsignedFloat(Float):
+    pass
+
+
+@Typed(str)
+class String(Descriptor):
+    pass
+
+
+@MaxSized
+class SizedString(String):
+    pass
